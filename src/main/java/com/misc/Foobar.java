9b2b850de2a4c188eaa910962db69ade4d0e0f2c
pkapcia <ptrkap@gmail.com>
2016-05-29 17:44:53 +0200

    another thing in science featue

--- src/main/java/com/misc/Foobar.java
+++ src/main/java/com/misc/Foobar.java
@@ -6,6 +6,7 @@ public class Foobar {
 
     public void calc2() {
         System.out.println("This is science feature");
+        System.out.println("another thing in science feature branch");
     }
 
     public void calc3() {
